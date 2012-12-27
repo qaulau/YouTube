dab568ffe5dadb2b7a8a5c81f250ff028aee6f8a
Qau Lau <qaulau@hotmail.com>
2012-12-28 02:53:54 +0800

    未完成，暂时保存!
    未完成，暂时保存!

--- youtube.php
+++ youtube.php
@@ -60,7 +60,6 @@ class YouTube
             return $info;
         }else
             return false;
-		//return $data;
 	}
 	//根据URL获取vid
 	private function getVidByUrl(){
@@ -72,17 +71,36 @@ class YouTube
 	{
 	    return 'http://www.youtube.com/watch?v='.$this->vid;
 	} 
-
-	private function getThumbnails()
+	
+	//获取播放地址
+	private function getVurl()
 	{
 	    
 	} 
-
-	private function getVideoUrl()
-	{
-	    
-	} 
-
+	
+	/**
+	 * 获取视频数据默认返回的是一个数组，如果为true的话则返回json数据
+	 * */
+	public function getVideoData($isJson = false){
+		$info = this->getVideoInfo();
+		$data = array(
+			'title' => $info['title'],				//视频标题
+			'keywords' => $info['keywords'],		//视频关键词
+			'seconds' => $info['length_seconds'],	//视频时间长度（单位秒）
+			'thumbnails' => array(					//视频缩略图
+				'big' => $info['iurlsd'],			//大图
+				'small' => $info['thumbnail_url']	//小图			
+			);
+			'vurl' => $this->getVurl();				//视频播放地址
+		);
+		
+		if($isJson)
+			return json_encode($data);
+		else
+			return $data;
+		
+	}
+	//调试信息
 	public function debug()
 	{
 		$debugMsg = '<b><font color="red">以下是调试信息:</font></b><br/>';
