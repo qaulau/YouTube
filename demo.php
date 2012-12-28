cef3bd0102ffefc506d57f96a47afd1df9498b49
Qau Lau <qaulau@hotmail.com>
2012-12-28 03:44:44 -0800

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,11 @@
+<?php
+
+require('./youtube.php');
+#简单的调试
+$url = $_GET['url'];
+if($url){
+  $yt = new YouTube($url); 
+}else{
+	$yt = new YouTube('http://www.youtube.be/watch?v=pAk1XIB9Ok0'); 
+}
+$yt->debug();
