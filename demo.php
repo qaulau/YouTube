fbef221e73920a7a4f42652b065a147958b1ada9
Qau Lau <qaulau@hotmail.com>
2012-12-28 19:59:13 +0800

    简单的演示

--- demo.php
+++ demo.php
@@ -2,9 +2,9 @@
 
 require('./youtube.php');
 #简单的调试
-$url = $_GET['url'];
+$v = $_GET['v'];
 if($url){
-	$yt = new YouTube($url); 
+	$yt = new YouTube($v); 
 }else{
 	$yt = new YouTube('http://www.youtube.com/watch?v=iQDm6XzgbPw&list=PL14BFF58590E9CE26'); 
 }
